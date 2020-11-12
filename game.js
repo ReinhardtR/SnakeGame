51628ab39a3d20f0ae2ff3f11501496c5544aaf8
Reinhardt Rijna <reinhardtrijna@gmail.com>
2020-11-12 22:42:46 +0100

    Needs GUI
    Plans:
    - Firebase Leaderboard

--- game.js
+++ game.js
@@ -7,6 +7,9 @@ var apple = new Apple();
 const gameBoard = document.getElementById("game-board");
 var lastRenderTime = 0;
 
+var isAlive = false;
+var score = 0;
+
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
@@ -14,24 +17,37 @@ function main(currentTime) {
 
   lastRenderTime = currentTime;
 
-  update();
-  draw();
+  if (isAlive) {
+    update();
+    draw();
+  } else {
+    window.addEventListener("keydown", () => {isAlive = true});
+  }
 }
 
 window.requestAnimationFrame(main);
 
 function update() {
+  console.log("ALIVE");
   snake.update();
   if (snake.collision(apple.position)) {
+    score++;
     snake.newSegments += apple.increaseAmount;
     while (snake.collision(apple.position)) {
       apple.spawn();
     }
   }
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   snake.draw(gameBoard);
   apple.draw(gameBoard);
+}
+
+function checkDeath() {
+  if (snake.body[0].x < 0 || snake.body[0].y < 0) {
+    isAlive = false;
+  }
 }
