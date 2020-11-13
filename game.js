b6742f538186ada46929c673ed8200b11c7bacdc
Reinhardt Rijna <reinhardtrijna@gmail.com>
2020-11-13 06:49:32 +0100

    GUI WIP

--- game.js
+++ game.js
@@ -4,31 +4,51 @@ import Apple from "./apple.js";
 var snake = new Snake();
 var apple = new Apple();
 
+const userInterface = document.getElementById("gui");
 const gameBoard = document.getElementById("game-board");
-var lastRenderTime = 0;
 
 var isAlive = false;
 var score = 0;
 
+if (isAlive) {
+  main();
+} else {
+  window.addEventListener("keydown", () => {
+    isAlive = true;
+    gameBoard.innerHTML = "";
+  });
+  GUI();
+}
+
+var lastRenderTime = 0;
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / snake.speed) return;
-
   lastRenderTime = currentTime;
 
   if (isAlive) {
     update();
     draw();
   } else {
-    window.addEventListener("keydown", () => {isAlive = true});
+    window.addEventListener("keydown", () => {
+      isAlive = true;
+      gameBoard.innerHTML = "";
+    });
+    GUI();
   }
 }
 
 window.requestAnimationFrame(main);
 
+function GUI() {
+  const guiElement = document.createElement("div");
+  guiElement.classList.add("gui");
+  guiElement.append("Press Enter to Play");
+  gameBoard.append(guiElement);
+}
+
 function update() {
-  console.log("ALIVE");
   snake.update();
   if (snake.collision(apple.position)) {
     score++;
